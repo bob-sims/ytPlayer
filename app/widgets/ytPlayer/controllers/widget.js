fd073a8d14a8445abe9dbf99389dd79a32490c9b
apotropaic <andrew@jesusculture.com>
2015-06-13 00:26:43 -0700

    Update widget.js
    updated for new youtube API

--- app/widgets/ytPlayer/controllers/widget.js
+++ app/widgets/ytPlayer/controllers/widget.js
@@ -1,94 +1,96 @@
-var win = null;
+//var ytPlayer = require (WPATH('ytPlayer'));
 
+//var lib = Alloy.Globals;
+ 
+var win = null
+ 
 exports.isPlaying = false;
-
+ 
 exports.play = function(id) {
+	console.info('id: ' + id);
     exports.isPlaying = true;
-
     getVideo(id);
 };
 
+function getArgs(string){
+	var args = {};
+	var pairs = string.split("&");
+	for(var i = 0; i < pairs.length; i++) {
+		var pos = pairs[i].indexOf('=');
+		if (pos == -1) continue;
+		var argname = pairs[i].substring(0,pos);
+		var value = pairs[i].substring(pos+1);
+		args[argname] = unescape(value);
+	}
+	return args;
+
+};
+ 
 function getVideo(id) {
-    var client = Ti.Network.createHTTPClient();
+	var youtubeInfoUrl = 'http://www.youtube.com/get_video_info?video_id=' + id;
+	var request = Titanium.Network.createHTTPClient({ timeout : 10000  /* in milliseconds */});
+	request.open("GET", youtubeInfoUrl);
+	request.onerror = function(event){
+	};
+	request.onload = function(event){
+		var qualities = {};
+		var response = this.responseText;
+		var args = getArgs(response);
 
-    client.onload = function() {
-        var json = (this.responseText.substring(4, this.responseText.length));
-        var response = JSON.parse(json);
+		if (!args.hasOwnProperty('url_encoded_fmt_stream_map')){
+		}
+		else{
+			var fmtstring = args['url_encoded_fmt_stream_map'];
+			var fmtarray = fmtstring.split(',');
+			for(var i=0,j=fmtarray.length; i<j; i++){
+				var args2 = getArgs(fmtarray[i]);
+				var type = decodeURIComponent(args2['type']);
+				// Ti.API.error("args ==== "+JSON.stringify(args2));
+				if (type.indexOf('mp4') >= 0){
+						var url = decodeURIComponent(args2['url']);
+						var quality = decodeURIComponent(args2['quality']);
+						qualities[quality] = url;
+				}
+			}
+			playVideo(qualities.medium);
+		}
+	};
 
-        if (response.result === 'error') {
-            exports.isPlaying = false;
+	request.send();
 
-            if (OS_ANDROID) {
-                $.videoPlayer = null;
-            }
-
-            alert(L('ytPlayer_videoNotFound', 'Video not found'));
-
-            return;
-        }
-
-        var isHighQuality = response.content['player_data']['fmt_stream_map'] !== null;
-        var streamUrl = isHighQuality ? response.content['player_data']['fmt_stream_map'][0].url : '';
-
-        if (!isHighQuality) {
-            Ti.API.debug('using low quality video because fmt_stream_map does not exist in json response, User-Agent probably is not being sent correctly');
-        }
-
-        playVideo(streamUrl);
-    };
-
-    if (OS_IOS) {
-        client.setRequestHeader('Referer', 'http://www.youtube.com/watch?v=' + id);
-        client.setRequestHeader('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/536.26.14 (KHTML, like Gecko) Version/6.0.1 Safari/536.26.14');
-    }
-
-    client.open('GET', 'http://m.youtube.com/watch?ajax=1&layout=mobile&tsp=1&utcoffset=330&v=' + id);
-
-    if (OS_ANDROID) {
-        client.setRequestHeader('Referer', 'http://www.youtube.com/watch?v=' + id);
-        client.setRequestHeader('User-Agent', 'Mozilla/5.0 (Linux; U; Android 2.2.1; en-gb; GT-I9003 Build/FROYO) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1');
-    }
-
-    client.send();
 }
-
+ 
 function playVideo(url) {
-
-    if (OS_IOS) {
-        win = Widget.createController('window').getView();
-    }
-
+   if(OS_IOS) {
+		win = Widget.createController('window').getView();
+   } 
     $.videoPlayer.setUrl(url);
-
-    $.videoPlayer.addEventListener('complete', function(e) {
+    $.videoPlayer.addEventListener('complete', function(e) { 
+        Ti.API.info('video player complete');
         exports.close();
     });
-
     $.videoPlayer.addEventListener('fullscreen', function(e) {
         if (!e.entering) {
+            Ti.API.info('video player fullscreen exit');
             exports.close();
         }
     });
-
-    if (OS_IOS) {
+    if(OS_IOS) {
         win.add($.videoPlayer);
-        win.open({
-            animated: false
-        });
+        win.open();
     }
 }
-
+ 
 exports.close = function() {
-
-    if (OS_IOS) {
+    Ti.API.info('closing video player');
+    if(OS_IOS) {
         $.videoPlayer.fullscreen = false;
         win.close();
-
-    } else {
+    }
+    else {
         $.videoPlayer.hide();
         $.videoPlayer.release();
         $.videoPlayer = null;
     }
-
     exports.isPlaying = false;
 };
